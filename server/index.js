a19cf15383251ff58d5e22f2f8040c43f5baa72b
Jonathan Tsai <modopo@gmail.com>
2024-02-14 18:37:36 -0800

    working connection to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import express from 'expres';
+import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
@@ -10,13 +10,10 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
-
-
-
 // Configs
 dotenv.config();
 const app = express();
-app.use(express.json);
+app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
 app.use(morgan("common"));
@@ -30,4 +27,11 @@ app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
 
-
+// mongoose
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+}).catch((error) => console.log(`${error} did not connect`))
