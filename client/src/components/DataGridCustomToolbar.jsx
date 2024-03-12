6a64b954b8ae3ebfc6d112b273175c7d630f6622
Jonathan Tsai <modopo@gmail.com>
2024-03-12 00:05:22 -0700

    data grid custom toolbar start

---
+++ client/src/components/DataGridCustomToolbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Search } from "@mui/icons-matieral";
+import { IconButton, TextField, InputAdornment } from "@mui/material";
+import {
+  GridToolbarDensitySelector,
+  GridToolbarContainer,
+  GridToolbarExport,
+  GridToolbarColumnsButton,
+} from "@mui/x-data-grid";
+import FlexBetween from "./FlexBetween.jsx";
+
+const DataGridCustomToolbar = () => {
+  return (
+    <GridToolbarContainer>
+      <FlexBetween>
+        <GridToolbarColumnsButton />
+        <GridToolbarDensitySelector />
+        <GridToolbarExport />
+      </FlexBetween>
+    </GridToolbarContainer>
+  );
+};
--- client/src/scenes/transactions/index.jsx
+++ client/src/scenes/transactions/index.jsx
@@ -86,6 +86,14 @@ const Transactions = () => {
           getRowId={(row) => row._id}
           rows={(data && data.transactions) || []}
           columns={columns}
+          rowCount={(data && data.total) || 0}
+          pagination
+          page={page}
+          paginationMode="server"
+          sortingMode="server"
+          onPageChange={(newPage) => setPage(newPage)}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          onSortModelChange={(newSortModel) => setSort(...newSortModel)}
         />
       </Box>
     </Box>
