b7bd47d3df82b9325fc4b28d85298ab84fccae3e
modopo <modopo@gmail.com>
2024-06-20 13:06:28 -0700

    removed html text

--- client/src/scenes/breakdown/index.jsx
+++ client/src/scenes/breakdown/index.jsx
@@ -7,7 +7,6 @@ const Breakdown = () => {
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="BREAKDOWN" subtitle="Breakdown of Sales by Category" />
-      />
       <Box mt="40px" height="75vh">
         <BreakdownChart />
       </Box>
