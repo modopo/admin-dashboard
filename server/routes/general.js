48cfc11c725141b22506f0456a93af2242b05765
modopo <modopo@gmail.com>
2024-06-20 18:42:27 -0700

    dashboard endpoint with mock current day/month/year

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUser } from "../controllers/general.js";
+import { getUser, getDashboardStats } from "../controllers/general.js";
 
 const router = express.Router();
 
 router.get("/user/:id", getUser);
+router.get("/dahsboard", getDashboardStats);
 
 export default router;
