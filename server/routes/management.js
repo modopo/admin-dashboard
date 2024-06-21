a055db9694d5410e2474b618f6908c79065ce325
modopo <modopo@gmail.com>
2024-06-20 17:13:55 -0700

    aggregate sales endpoint

--- server/routes/management.js
+++ server/routes/management.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAdmins } from "../controllers/management.js";
+import { getAdmins, getUswerPerformance } from "../controllers/management.js";
 
 const router = express.Router();
 
 router.get("/admins", getAdmins);
+router.get("/perforamce/:id", getUserPerformance);
 
 export default router;
