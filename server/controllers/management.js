3896a50937b220ccf28c62d84be7c6fc8ac25658
modopo <modopo@gmail.com>
2024-06-20 17:50:52 -0700

    performance displayed in datagrid

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
-import Transaction fromn "../models/Transaction.js";
+import Transaction from "../models/Transaction.js";
 
 export const getAdmins = async (req, res) => {
   try {
@@ -14,30 +14,33 @@ export const getAdmins = async (req, res) => {
 export const getUserPerformance = async (req, res) => {
   try {
     const { id } = req.params;
+
     const userWithStats = await User.aggregate([
-      { $match: { _id: new mongoose.Types.ObjectId(id) }},
+      { $match: { _id: new mongoose.Types.ObjectId(id) } },
       {
         $lookup: {
           from: "affiliatestats",
           localField: "_id",
           foreignField: "userId",
-          as: "affiliateStats"
-        }
+          as: "affiliateStats",
+        },
       },
       { $unwind: "$affiliateStats" },
-    ])
+    ]);
 
     const saleTransactions = await Promise.all(
       userWithStats[0].affiliateStats.affiliateSales.map((id) => {
         return Transaction.findById(id);
-      })
-    )
-    const filteredSales = saleTransactions.filter(
-      (transaction) => transaction !== null;
+      }),
+    );
+    const filteredSaleTransactions = saleTransactions.filter(
+      (transaction) => transaction !== null,
     );
 
-    res.status(200).json({ user: userWithStats[0], sales: filteredSales });
+    res
+      .status(200)
+      .json({ user: userWithStats[0], sales: filteredSaleTransactions });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
-}
+};
--- server/routes/management.js
+++ server/routes/management.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getAdmins, getUswerPerformance } from "../controllers/management.js";
+import { getAdmins, getUserPerformance } from "../controllers/management.js";
 
 const router = express.Router();
 
 router.get("/admins", getAdmins);
-router.get("/perforamce/:id", getUserPerformance);
+router.get("/performance/:id", getUserPerformance);
 
 export default router;
