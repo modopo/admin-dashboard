2fb70e47fb5056c83388c89e3740ef4254552514
modopo <modopo@gmail.com>
2024-06-25 17:01:53 -0700

    fixed syntax and typos

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -15,8 +15,8 @@ export const register = async (req, res) => {
       occupation,
       phoneNumber,
     } = req.body;
-    
-    const salt = await.bcrypt.genSalt();
+
+    const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
     const newUser = new User({
       name,
@@ -34,4 +34,21 @@ export const register = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
-}
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched) return res.status(400).json({ msg: "Invalid credential" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
